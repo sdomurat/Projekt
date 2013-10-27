abeac66601778e8b85c78d591395862bc8ad6965
asd <asd>
2013-10-27 14:25:44 +0100

    Początki serwera w NODE JS

---
+++ server.js
@@ -0,0 +1,37 @@
+var server = http.createServer(function (req, res) {
+	'use strict';
+    var filePath = '.' + req.url,
+        contentType = 'text/html',
+        extName;
+    console.log('request starting...');
+    if (filePath === './') {
+        filePath = './stol.html';
+    }
+    extName = path.extname(filePath);
+    switch (extName) {
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.css':
+        contentType = 'text/css';
+        break;
+    }
+
+    path.exists(filePath, function (exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function (error, content) {
+                if (error) {
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                }
+            });
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
+    });
+});
