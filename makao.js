e16059d8b929b87b64f6a43a1d18c6caf94f567e
Szymon <asd>
2013-10-28 15:31:30 +0100

    metoda dla ustaw zadanie figury

--- makao.js
+++ makao.js
@@ -10,4 +10,21 @@ var socket = io.connect(window.location.hostname),
 $(document).ready(function () {
 	var rK, kartyDoRzucenia = [], zaznacz, kartyGracza = [], ustawCzekanie, errorInfo, makaoInfo,
 		figuryDoZadania, uZF, uZK, koloryDoZadania, entry_el, trwaGra, blokada = false;
+		
+	uZF = function (e) {//ustaw zadanie figury
+		var divZadanie = document.querySelector('div#zadanie');
+		if (e.target.id === 'nic') {
+			socket.emit('ustawZadanaFigure', 'nic');
+			zadanieF = false;
+			socket.emit('usunZadanaFigure');
+		} else {
+			socket.emit('ustawZadanaFigure', e.target.id);
+		}
+		socket.emit('uPZFNF'); //ustaw przeciwnikom zadanie figury na false
+		socket.emit('rzucKarte', kartyDoRzucenia);
+		divZadanie.innerHTML = '';
+		divZadanie.style.display = '';
+		blokada = false;
+	};
+	
 };	
